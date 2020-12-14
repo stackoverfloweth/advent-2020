7e3aba7560c9869f2a48c40c4a0e7e2496a7e759
Evan Sutherland <stackoverfloweth@gmail.com>
2020-12-14 09:41:46 -0600

    day 13 part 2 pass

--- day_13.js
+++ day_13.js
@@ -1,14 +1,7 @@
 const sample = [7, 13, 'x', 'x', 59, 'x', 31, 19]
 const input = [37, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 41, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 587, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 13, 19, 'x', 'x', 'x', 23, 'x', 'x', 'x', 'x', 'x', 29, 'x', 733, 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 17]
 
-// console.log(1068781 % 7)
-// console.log((1068781 + 1) % 13)
-// console.log((1068781 + 4) % 59)
-// console.log((1068781 + 6) % 31)
-// console.log((1068781 + 7) % 19)
-
 const max = input.reduce((reduced, x, index) => x != 'x' && x > reduced.max ? {index, max:x} : reduced, {index: 0, max: 0})
-//console.log(max)
 let time = Math.floor(100000000000000 / max.max) * max.max
 while (!setComplete(time)) {
     time += max.max
