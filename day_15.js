dc890d261316ae315e2322d78ae33e6d054a2d5c
Evan Sutherland <stackoverfloweth@gmail.com>
2020-12-16 09:43:13 -0600

    day 15 part 2 pass

--- day_15.js
+++ day_15.js
@@ -1,20 +1,14 @@
 const sample = [0, 3, 6]
 const input = [0, 14, 6, 20, 1, 4]
-const history = input.slice(0, -1).reduce((reduced, x) => ({ ...reduced, [x]: null }), {})
+const history = new Map()
 let lastNumber = input.shift()
 let index = 1
 
 while (index < 30000000) {
     const nextNumber = getNextNumber()
-    history[lastNumber] = index++
-
-    //console.log(nextNumber, history)
+    history.set(lastNumber, index++)
 
     lastNumber = nextNumber
-
-    if (index % 10000 == 0) {
-        console.log(index)
-    }
 }
 
 console.log(lastNumber)
@@ -24,12 +18,9 @@ function getNextNumber() {
         return input.shift()
     }
 
-    if (history[lastNumber] == undefined) {
-        history[lastNumber] = null
+    if (!history.has(lastNumber)) {
         return 0
     }
 
-    //console.log(index, history[lastNumber][history[lastNumber].length - 1])
-
-    return index - history[lastNumber]
+    return index - history.get(lastNumber)
 }
