4371d3958331a3766d5db2a43bd7a8fbe667416a
Evan Sutherland <stackoverfloweth@gmail.com>
2020-12-19 21:20:51 -0600

    day 17 part 1 pass

--- day_17.js
+++ day_17.js
@@ -1,14 +1,29 @@
+// const input = [
+//     '.#.',
+//     '..#',
+//     '###',
+// ]
 const input = [
-    '.#.',
-    '..#',
-    '###',
+    '#####...',
+    '.#..##..',
+    '##.##.##',
+    '...####.',
+    '#.#...##',
+    '.##...#.',
+    '.#.#.###',
+    '#.#.#..#',
 ]
 
 //z = y = x
 const cycles = [[input.map(x => x.split(''))]]
 
 execute();
-console.log(mostRecentCycle())
+execute();
+execute();
+execute();
+execute();
+execute();
+console.log(JSON.stringify(mostRecentCycle()).match(/\#/g).length)
 
 function execute() {
     const lastCycle = mostRecentCycle()
@@ -16,15 +31,22 @@ function execute() {
 
     for (let z = -1; z < lastCycle.length + 1; z++) {
         nextCycle.push([])
-        
+
         for (let y = -1; y < lastCycle[0].length + 1; y++) {
             nextCycle[z + 1].push([])
 
             for (let x = -1; x < lastCycle[0][0].length + 1; x++) {
-                const point = {x,y,z}
+                const point = { x, y, z }
                 const cell = getCell(lastCycle, point)
-                
-                nextCycle[z + 1][y + 1].push(cell.active ? '#' : '.')
+                const report = getAdjacentReport(lastCycle, point).filter(x => x.active)
+
+                if (cell.active && report.length >= 2 && report.length <= 3) {
+                    nextCycle[z + 1][y + 1].push("#")
+                } else if (!cell.active && report.length == 3) {
+                    nextCycle[z + 1][y + 1].push("#")
+                } else {
+                    nextCycle[z + 1][y + 1].push(".")
+                }
             }
         }
     }
@@ -32,11 +54,31 @@ function execute() {
     cycles.push(nextCycle)
 }
 
-function getCell(cycle, {x,y,z}){
+function getAdjacentReport(cycle, point) {
+    const report = []
+
+    for (let z = point.z - 1; z <= point.z + 1; z++) {
+        for (let y = point.y - 1; y <= point.y + 1; y++) {
+            for (let x = point.x - 1; x <= point.x + 1; x++) {
+                if (z == point.z && y == point.y && x == point.x) {
+                    continue
+                }
+
+                const cell = getCell(cycle, { x, y, z })
+
+                report.push(cell)
+            }
+        }
+    }
+
+    return report
+}
+
+function getCell(cycle, { x, y, z }) {
     try {
-        return {x, y, z, active: cycle[z][y][x] === "#"}
-    }catch {
-        return {x, y, z, active: false}
+        return { x, y, z, active: cycle[z][y][x] === "#" }
+    } catch (e) {
+        return { x, y, z, active: false }
     }
 }
 
