4ae65b81c1d8ddc6a56ecc2fa31ea48713f3d136
Evan Sutherland <stackoverfloweth@gmail.com>
2020-12-19 21:34:13 -0600

    day 17 part 2 pass

--- day_17.js
+++ day_17.js
@@ -14,8 +14,8 @@ const input = [
     '#.#.#..#',
 ]
 
-//z = y = x
-const cycles = [[input.map(x => x.split(''))]]
+//w = z = y = x
+const cycles = [[[input.map(x => x.split(''))]]]
 
 execute();
 execute();
@@ -29,23 +29,27 @@ function execute() {
     const lastCycle = mostRecentCycle()
     const nextCycle = []
 
-    for (let z = -1; z < lastCycle.length + 1; z++) {
+    for (let w = -1; w < lastCycle.length + 1; w++) {
         nextCycle.push([])
 
-        for (let y = -1; y < lastCycle[0].length + 1; y++) {
-            nextCycle[z + 1].push([])
+        for (let z = -1; z < lastCycle[0].length + 1; z++) {
+            nextCycle[w + 1].push([])
 
-            for (let x = -1; x < lastCycle[0][0].length + 1; x++) {
-                const point = { x, y, z }
-                const cell = getCell(lastCycle, point)
-                const report = getAdjacentReport(lastCycle, point).filter(x => x.active)
+            for (let y = -1; y < lastCycle[0][0].length + 1; y++) {
+                nextCycle[w + 1][z + 1].push([])
 
-                if (cell.active && report.length >= 2 && report.length <= 3) {
-                    nextCycle[z + 1][y + 1].push("#")
-                } else if (!cell.active && report.length == 3) {
-                    nextCycle[z + 1][y + 1].push("#")
-                } else {
-                    nextCycle[z + 1][y + 1].push(".")
+                for (let x = -1; x < lastCycle[0][0][0].length + 1; x++) {
+                    const point = { w, x, y, z }
+                    const cell = getCell(lastCycle, point)
+                    const report = getAdjacentReport(lastCycle, point).filter(r => r.active)
+
+                    if (cell.active && report.length >= 2 && report.length <= 3) {
+                        nextCycle[w + 1][z + 1][y + 1].push("#")
+                    } else if (!cell.active && report.length == 3) {
+                        nextCycle[w + 1][z + 1][y + 1].push("#")
+                    } else {
+                        nextCycle[w + 1][z + 1][y + 1].push(".")
+                    }
                 }
             }
         }
@@ -57,16 +61,18 @@ function execute() {
 function getAdjacentReport(cycle, point) {
     const report = []
 
-    for (let z = point.z - 1; z <= point.z + 1; z++) {
-        for (let y = point.y - 1; y <= point.y + 1; y++) {
-            for (let x = point.x - 1; x <= point.x + 1; x++) {
-                if (z == point.z && y == point.y && x == point.x) {
-                    continue
+    for (let w = point.w - 1; w <= point.w + 1; w++) {
+        for (let z = point.z - 1; z <= point.z + 1; z++) {
+            for (let y = point.y - 1; y <= point.y + 1; y++) {
+                for (let x = point.x - 1; x <= point.x + 1; x++) {
+                    if (w == point.w && z == point.z && y == point.y && x == point.x) {
+                        continue
+                    }
+
+                    const cell = getCell(cycle, { w, x, y, z })
+
+                    report.push(cell)
                 }
-
-                const cell = getCell(cycle, { x, y, z })
-
-                report.push(cell)
             }
         }
     }
@@ -74,11 +80,11 @@ function getAdjacentReport(cycle, point) {
     return report
 }
 
-function getCell(cycle, { x, y, z }) {
+function getCell(cycle, { w, x, y, z }) {
     try {
-        return { x, y, z, active: cycle[z][y][x] === "#" }
+        return { w, x, y, z, active: cycle[w][z][y][x] === "#" }
     } catch (e) {
-        return { x, y, z, active: false }
+        return { w, x, y, z, active: false }
     }
 }
 
